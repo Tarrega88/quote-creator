fa2f6a28d4ae0ff093d424fa71e699feaeb2de9a
Michael <michaelseedev@gmail.com>
2025-03-12 02:32:53 -0800

    done button in measurementCost

--- src/pages/expenses/materials/MeasurementCost.jsx
+++ src/pages/expenses/materials/MeasurementCost.jsx
@@ -4,6 +4,8 @@ import InstructionText from "../../../ui/InstructionText";
 import { useParams } from "react-router-dom";
 import NumberInput from "../../../ui/NumberInput";
 import { useState } from "react";
+import Subtitle from "../../../ui/Subtitle";
+import AcceptButton from "../../../ui/AcceptButton";
 
 function MeasurementCost() {
   const [dollars, setDollars] = useState(0);
@@ -23,18 +25,22 @@ function MeasurementCost() {
   return (
     <MainFlexContainer back={back}>
       <InstructionText text="How much does it cost?" />
-      <div>
-        <div>
-          It costs
-          <NumberInput
-            // value={dollars}
-            onChange={(e) => setDollars(e.target.value)}
-          />
-          dollars per{" "}
-          <NumberInput onChange={(e) => setUnitAmount(e.target.value)} />
+      {/* <Subtitle text="(Try to enter the smallest amount you're able to buy it in)" /> */}
+      {/* <div> */}
+      <div className="flex gap-2">
+        <span>It costs</span>
+        <NumberInput
+          // value={dollars}
+          onChange={(e) => setDollars(e.target.value)}
+        />
+        <span>dollars per</span>
+        <NumberInput onChange={(e) => setUnitAmount(e.target.value)} />
+        <span>
           {measurementUnit} of {materialName}
-        </div>
+        </span>
       </div>
+      {/* </div> */}
+      <AcceptButton isActive={dollars > 0 && unitAmount > 0} />
     </MainFlexContainer>
   );
 }
