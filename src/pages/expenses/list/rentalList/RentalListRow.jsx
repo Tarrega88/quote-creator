dd55e32ef7a57ac1776d00b4b3a3badeb733d2f7
Michael <michaelseedev@gmail.com>
2025-03-13 20:44:36 -0800

    rental list

--- src/pages/expenses/list/rentalList/RentalListRow.jsx
+++ src/pages/expenses/list/rentalList/RentalListRow.jsx
@@ -1,5 +1,29 @@
+import { useDispatch } from "react-redux";
+import { deleteExpense } from "../../../../store/slices/expenseSlice";
+import RowDelete from "../RowDelete";
+
 function RentalListRow({ data, odd }) {
-  return <div></div>;
+  const { expenseName, expenseType, costInDollars, paidByThe } = data;
+  const dispatch = useDispatch();
+
+  function handleDelete() {
+    dispatch(
+      deleteExpense({ expenseType: expenseType, expenseName: expenseName }),
+    );
+  }
+
+  const bgColor = odd ? "bg-slate-600" : "bg-slate-700";
+  const flatOrTimeText =
+    paidByThe === "flat" ? "flat rate" : `per ${paidByThe}`;
+
+  return (
+    <div className={`grid grid-cols-4 ${bgColor} py-1 pl-4`}>
+      <span>{expenseName}</span>
+      <span>{costInDollars}</span>
+      <span>{flatOrTimeText}</span>
+      <RowDelete onClick={handleDelete} />
+    </div>
+  );
 }
 
 export default RentalListRow;
