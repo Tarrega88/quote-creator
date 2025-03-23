6c45a4a67177e020c698c03995a0986fb8789166
Michael <michaelseedev@gmail.com>
2025-03-23 09:33:26 -0800

    adding dummyData to quote

--- src/pdfview/expenses/ExpenseRow.jsx
+++ src/pdfview/expenses/ExpenseRow.jsx
@@ -1,5 +1,18 @@
-function ExpenseRow() {
-  return <div></div>;
+import { StyleSheet, Text, View } from "@react-pdf/renderer";
+
+const styles = StyleSheet.create({
+  odd: { backgroundColor: "#333333" },
+  even: { backgroundColor: "#481382" },
+  text: {},
+});
+
+function ExpenseRow({ odd }) {
+  const styleChoice = odd ? styles.odd : styles.even;
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
 }
 
 export default ExpenseRow;
--- src/store/slices/clientSlice.js
+++ src/store/slices/clientSlice.js
@@ -16,15 +16,132 @@ const dummyData = {
             quotes: {
                 1742300944160: {
                     dateCreated: 1742300944160,
-                    expenses: {
-                        materials: {},
-                        labor: {},
-                        rentals: {},
+                    "expenses": {
+                        "materials": {
+                            "e74bb82a-df6e-4680-af2d-fb65ad15e554": {
+                                "costInDollars": 22.7,
+                                "expenseName": "fabric",
+                                "expenseType": "materials",
+                                "measurementType": "length",
+                                "measurementUnit": "feet",
+                                "paymentModel": "measurement",
+                                "unitAmount": 10,
+                                "multiple": 1,
+                                "category": "expenses",
+                                "uuid": "e74bb82a-df6e-4680-af2d-fb65ad15e554"
+                            },
+                            "b4e8a14f-c7af-4255-9c43-6d748a966b7f": {
+                                "costInDollars": 40,
+                                "expenseName": "sheet metal",
+                                "expenseType": "materials",
+                                "measurementType": "area",
+                                "measurementUnit": "square feet",
+                                "paymentModel": "measurement",
+                                "unitAmount": 50,
+                                "multiple": 1,
+                                "category": "expenses",
+                                "uuid": "b4e8a14f-c7af-4255-9c43-6d748a966b7f"
+                            }
+                        },
+                        "labor": {
+                            "9ef02b8e-91f4-4c4e-ab29-2a886f24e49a": {
+                                "costInDollars": 450,
+                                "expenseName": "general labor",
+                                "expenseType": "labor",
+                                "paymentModel": "time",
+                                "paidByThe": "day",
+                                "multiple": 1,
+                                "category": "expenses",
+                                "uuid": "9ef02b8e-91f4-4c4e-ab29-2a886f24e49a"
+                            },
+                            "9cdbba90-93ab-425a-8b76-d72a144a7768": {
+                                "costInDollars": 450,
+                                "expenseName": "general labor",
+                                "expenseType": "labor",
+                                "paymentModel": "time",
+                                "paidByThe": "day",
+                                "multiple": 2,
+                                "category": "expenses",
+                                "uuid": "9cdbba90-93ab-425a-8b76-d72a144a7768"
+                            }
+                        },
+                        "rentals": {
+                            "6c440a10-fb48-461c-8e8d-65b4f72724dd": {
+                                "costInDollars": 25,
+                                "expenseName": "Uhaul",
+                                "expenseType": "rentals",
+                                "paymentModel": "time",
+                                "paidByThe": "day",
+                                "multiple": 1,
+                                "category": "expenses",
+                                "uuid": "6c440a10-fb48-461c-8e8d-65b4f72724dd"
+                            },
+                            "d286489d-c310-458c-8815-598adceff21a": {
+                                "costInDollars": 25,
+                                "expenseName": "Uhaul",
+                                "expenseType": "rentals",
+                                "paymentModel": "time",
+                                "paidByThe": "day",
+                                "multiple": 1,
+                                "category": "expenses",
+                                "uuid": "d286489d-c310-458c-8815-598adceff21a"
+                            }
+                        }
                     },
-                    services: {
-                        //category, like installs gets added when a service is added to quote
-                        //installs: [],
-                    },
+                    "services": {
+                        "installation": {
+                            "30245546-d5a3-48f9-8bb1-34fe5fedae06": {
+                                "measurementType": "area",
+                                "measurementUnit": "square_feet",
+                                "pay": 500,
+                                "paymentModel": "measurement",
+                                "serviceCategory": "installation",
+                                "serviceName": "French drain installation",
+                                "unitAmount": 2,
+                                "multiple": 1,
+                                "category": "services",
+                                "uuid": "30245546-d5a3-48f9-8bb1-34fe5fedae06"
+                            },
+                            "963ebeb5-27bb-4069-ae26-81d1b70a7de7": {
+                                "measurementType": "area",
+                                "measurementUnit": "square_feet",
+                                "pay": 450,
+                                "paymentModel": "measurement",
+                                "serviceCategory": "installation",
+                                "serviceName": "English drain installation",
+                                "unitAmount": 2,
+                                "multiple": 1,
+                                "category": "services",
+                                "uuid": "963ebeb5-27bb-4069-ae26-81d1b70a7de7"
+                            }
+                        },
+                        "landscaping": {
+                            "2f0ce417-06fa-49b0-b4c9-c30110b6a187": {
+                                "measurementType": "area",
+                                "measurementUnit": "square_feet",
+                                "pay": 250,
+                                "paymentModel": "measurement",
+                                "serviceCategory": "landscaping",
+                                "serviceName": "grading",
+                                "unitAmount": 40,
+                                "multiple": 1,
+                                "category": "services",
+                                "uuid": "2f0ce417-06fa-49b0-b4c9-c30110b6a187"
+                            },
+                            "94c3ace7-faf5-4b5e-84cc-26b50041a995": {
+                                "measurementType": "area",
+                                "measurementUnit": "square_feet",
+                                "pay": 250,
+                                "paymentModel": "measurement",
+                                "serviceCategory": "landscaping",
+                                "serviceName": "grading",
+                                "unitAmount": 40,
+                                "multiple": 1,
+                                "category": "services",
+                                "uuid": "94c3ace7-faf5-4b5e-84cc-26b50041a995"
+                            }
+                        }
+                    }
                 },
                 1742410954170: {
                     dateCreated: 1742410954170,
